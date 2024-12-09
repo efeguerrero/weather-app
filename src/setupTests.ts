59281eb94770c43f9d78535eda005cf3087a55ab
efeguerrero <juanfg.8640@gmail.com>
2024-12-09 17:56:12 -0300

    Added workaround in setupTests to avoid React Router future flag warning

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -4,5 +4,18 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 
+beforeAll(() => {
+  jest
+    .spyOn(console, 'warn')
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    .mockImplementation((msg: string, ...args: any[]) => {
+      if (!msg.includes('React Router Future Flag Warning')) {
+        // eslint-disable-next-line no-console
+        console.warn(msg, ...args);
+      }
+    });
+});
 
-
+afterAll(() => {
+  jest.restoreAllMocks();
+});
